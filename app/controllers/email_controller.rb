d0e2bec69405c120890acc24ae2c98e7ed18ab8f
joeymariano <joey.michalina.mariano@gmail.com>
2019-06-10 22:50:35 -0400

    took out a pry

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,6 +1,5 @@
 class EmailController < ApplicationController
   def create
-    binding.pry
     c = ClientForm.new(:name => params['name'], :surname => params['surname'], :email => params['email'], :topic => params['topic'], :message => params['message'])
     if c.deliver
       flash[:notice] = "Email Sent Successfully."
