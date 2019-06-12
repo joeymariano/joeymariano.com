2dcc47b9cf26bee1223fd1a130d5044baf799c23
joeymariano <joey.michalina.mariano@gmail.com>
2019-06-12 12:55:37 -0400

    refactored carousel for iteration - detects folder and link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,28 @@ class WelcomeController < ApplicationController
   end
 
   def art
+    # turn links into an array :)
+    @pixel_art = []
+    @digital_art = []
+    @physical_art = []
+    @ready_run = []
+
+    Dir.foreach('app/assets/images/pixel_art') do |item|
+      next if item == '.' or item == '..'
+      @pixel_art << "assets/pixel_art/" + item
+    end
+    Dir.foreach('app/assets/images/digital_art') do |item|
+      next if item == '.' or item == '..'
+      @digital_art << "assets/digital_art/" + item
+    end
+    Dir.foreach('app/assets/images/physical_art/') do |item|
+      next if item == '.' or item == '..'
+      @physical_art << "assets/physical_art/" + item
+    end
+    Dir.foreach('app/assets/images/ready_run') do |item|
+      next if item == '.' or item == '..'
+      @ready_run << "assets/ready_run/" + item
+    end
   end
 
   def resume
