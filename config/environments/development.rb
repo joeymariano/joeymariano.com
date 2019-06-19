2e55e2ec47545c45ea00e80b0aadb496cd46ec55
joeymariano <joey.michalina.mariano@gmail.com>
2019-06-19 05:27:51 -0400

    fixed config variables for mailform

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,10 @@ Rails.application.configure do
   # SMTP settings for mailgun
   config.action_mailer.smtp_settings = {
     :port           => 587,
-    :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
-    :user_name      => ENV['username'],
-    :password       => ENV['password'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :domain         => ENV['MAILGUN_DOMAIN'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :plain
   }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,13 @@ Rails.application.configure do
   # SMTP settings for mailgun
   config.action_mailer.smtp_settings = {
     :port           => 587,
-    :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
-    :user_name      => ENV['username'],
-    :password       => ENV['password'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :domain         => ENV['MAILGUN_DOMAIN'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :plain
   }
-  
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
