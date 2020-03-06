7b41f9a2847cfdb33f9344d9d669836a5071087d
joeymariano <joey.michalina.mariano@gmail.com>
2020-03-06 15:29:46 -0500

    added hcaptcha and httparty gem - need to work on backend

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,5 +1,9 @@
 class EmailController < ApplicationController
   def create
+    binding.pry
+    # verify captcha with http request
+    # if request is good do the following
+
     c = ClientForm.new(:name => params['name'], :surname => params['surname'], :email => params['email'], :topic => params['topic'], :message => params['message'])
     if c.deliver
       flash[:notice] = "Email Sent Successfully."
@@ -9,4 +13,5 @@ class EmailController < ApplicationController
       redirect_to '/contact'
     end
   end
+
 end
